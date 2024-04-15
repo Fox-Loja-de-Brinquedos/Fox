606c75da91a2f3a42b4ee130e1b53ec80b2de7e8
lucasilmor <lucassilmor0@hotmail.com>
2024-04-15 02:25:18 -0300

    css da tela de pedidos

--- resources/views/perfil/orderList.blade.php
+++ resources/views/perfil/orderList.blade.php
@@ -16,6 +16,7 @@
       <li class="nav-item"><img src="images/fox1.svg" alt="" width="116px" height="122px"></li>
       <li class="nav-item"><img src="images/seguro.png" alt="" width="23px" height="20px">Ambiente 100% seguro</li>
     </ul>
+    <hr>
   </header>
 
     <main>
@@ -26,28 +27,27 @@
 
     <div class="orders-section">
         <div class="order">
-            <p>EM ANDAMENTO</p>
-            <p>#00001</p>
-            <img src="/" alt="miniImg">
-            <p>data do pedido: 01/02/2024</p>
-            <p>Unidade: 1</p>
-            <p>R$ 1010,15</p>
-            <div class="progress-bar"></div>
+            <p class="status-order">EM ANDAMENTO</p>
+            <p class="item-order">#00001</p>
+            <img class="img-order" src="images/toy.png" alt="miniImg">
+            <p class="date-order">Data do pedido: 01/02/2024</p>
+            <p class="order-unit">Unidade: 1</p>
+            <p class="order-price">R$ 1010,15</p>
+            <div class="progress-bar open"> </div>
         </div>
-<hr>
+
         <div class="order">
-            <p>EM ANDAMENTO</p>
-            <p>#00001</p>
-            <img src="/" alt="miniImg">
-            <p>data do pedido: 01/02/2024</p>
-            <p>Unidade: 1</p>
-            <p>R$ 1010,15</p>
-            <div class="progress-bar"></div>
+            <p class="status-order">FINALIZADO</p>
+            <p class="item-order">#00666</p>
+            <img class="img-order" src="images/toy.png" alt="miniImg">
+            <p class="date-order">Data do pedido: 02/01/2023</p>
+            <p class="order-unit">Unidade: 2</p>
+            <p class="order-price">R$ 110,15</p>
+            <div class="progress-bar close"> </div>
         </div>
 
         </div>
     </main>
-<hr>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>

16acda00719a4c1a240746b2bb0712783f24c01e
lucasilmor <lucassilmor0@hotmail.com>
2024-05-11 21:34:18 -0300

    sessao de editar perfil finalizada, com metodos de alterar senha e email com mensagens de erro personalizada

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,17 @@ Route::get("/", [produtoController::class,"index"])->name('produto');
 
 
 Route::middleware('auth')->group(function () {
-Route::get('/profile', [profileController::class, 'index'])->name('profile');
+Route::get('/profile', [profileController::class, 'create'])->name('profile');
+
+Route::post('/updateProfileName', [profileController::class, 'updateProfileName'])->name('update.name');
+Route::post('/updateProfileEmail', [profileController::class, 'updateProfileEmail'])->name('update.email');
+Route::post('/updateProfilePassword', [profileController::class, 'updateProfilePassword'])->name('update.password');
+
 Route::get('/address', [profileController::class, 'showAddress'])->name('address');
+
 Route::get('/accountDetails', [profileController::class, 'showAccountDetails'])->name('accountDetails');
+
+
 Route::get('/orderList', [orderController::class, 'index'])->name('orderList');
 
 

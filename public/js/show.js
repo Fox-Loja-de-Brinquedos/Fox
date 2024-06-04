16c18b0423cb7d3e0f9831e2613c0cf1a544875f
Rafa <rafaelleotsuka@gmail.com>
2024-06-03 21:17:37 -0300

    efeito zoom na imagem

--- public/js/show.js
+++ public/js/show.js
@@ -58,3 +58,28 @@ Array.from(thumbnails).forEach(function(thumbnail) {
         mainImg.src = this.src;
     });
 });
+
+//zoom da imagem 
+document.addEventListener('DOMContentLoaded', function () {
+    const mainImageContainer = document.querySelector('.main-image');
+    const mainImage = document.querySelector('.main-image img');
+
+    mainImageContainer.addEventListener('mouseenter', function () {
+      mainImageContainer.classList.add('zoomed');
+    });
+
+    mainImageContainer.addEventListener('mousemove', function (e) {
+      const rect = mainImageContainer.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      const xPercent = (x / rect.width) * 100;
+      const yPercent = (y / rect.height) * 100;
+
+      mainImage.style.transformOrigin = `${xPercent}% ${yPercent}%`;
+    });
+
+    mainImageContainer.addEventListener('mouseleave', function () {
+      mainImageContainer.classList.remove('zoomed');
+    });
+  });

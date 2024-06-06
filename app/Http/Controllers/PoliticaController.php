fd8583801978a67f895efd0888db7a54a5820720
Gabriel Oka <gabukabr@gmail.com>
2024-06-05 21:59:11 -0300

    telas de políticas e sobre responsivas

--- app/Http/Controllers/PoliticaController.php
+++ app/Http/Controllers/PoliticaController.php
@@ -3,20 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Carrinho;
 class PoliticaController extends Controller
 {
     public function politicaDePrivacidade()
     {
-        return view('politicas.politica-de-privacidade');
+        // Usuário ativo
+        $usuarioId = auth()->id();
+
+        // Conta o número de produtos diferentes no carrinho do usuário logado onde a quantidade do item é maior que 0
+        $qtdItensCarinho = Carrinho::where('USUARIO_ID', $usuarioId)
+            ->where('ITEM_QTD', '>', 0)
+            ->distinct('PRODUTO_ID')
+            ->count('PRODUTO_ID');
+
+        return view('politicas.politica-de-privacidade', [
+            'qtdItensCarinho' => $qtdItensCarinho
+        ]);
     }
 
     public function sobreNos()
     {
-        return view('politicas.sobre-nos');
+        // Usuário ativo
+        $usuarioId = auth()->id();
+
+        // Conta o número de produtos diferentes no carrinho do usuário logado onde a quantidade do item é maior que 0
+        $qtdItensCarinho = Carrinho::where('USUARIO_ID', $usuarioId)
+            ->where('ITEM_QTD', '>', 0)
+            ->distinct('PRODUTO_ID')
+            ->count('PRODUTO_ID');
+
+        return view('politicas.sobre-nos', [
+            'qtdItensCarinho' => $qtdItensCarinho
+        ]);
     }
 
     public function trocasDevolucoes()
     {
-        return view('politicas.trocas-devolucoes');
+        // Usuário ativo
+        $usuarioId = auth()->id();
+
+        // Conta o número de produtos diferentes no carrinho do usuário logado onde a quantidade do item é maior que 0
+        $qtdItensCarinho = Carrinho::where('USUARIO_ID', $usuarioId)
+            ->where('ITEM_QTD', '>', 0)
+            ->distinct('PRODUTO_ID')
+            ->count('PRODUTO_ID');
+
+        return view('politicas.trocas-devolucoes', [
+            'qtdItensCarinho' => $qtdItensCarinho
+        ]);
     }
 }
